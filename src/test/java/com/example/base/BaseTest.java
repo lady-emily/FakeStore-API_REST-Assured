1730f338dcc0f4847f57802c5209889cfc296e3e
ladyemilyy <equarsh33@gmail.com>
2025-07-14 13:12:57 +0000

    restore yaml

--- src/test/java/com/example/base/BaseTest.java
+++ src/test/java/com/example/base/BaseTest.java
@@ -1,25 +1,25 @@
-//package com.example.base;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.filter.log.LogDetail;
-//import io.restassured.specification.RequestSpecification;
-//import org.testng.annotations.BeforeClass;
-//
-//import static io.restassured.RestAssured.given;
-//
-//public class BaseTest {
-//
-//    @BeforeClass
-//    public void setUp() {
-//        String baseUrl = System.getenv("BASE_URL");
-//        if (baseUrl == null || baseUrl.isEmpty()) {
-//            baseUrl = "https://fakestoreapi.com"; // fallback if not set
-//        }
-//        RestAssured.baseURI = baseUrl;
-//        System.out.println("Base URI set to: " + RestAssured.baseURI);
-//
-//        RestAssured.requestSpecification = RestAssured
-//                .given()
-//                .header("User-Agent", "Mozilla/5.0 (compatible; GitHub Actions)");
-//    }
-//}
+package com.example.base;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setUp() {
+        String baseUrl = System.getenv("BASE_URL");
+        if (baseUrl == null || baseUrl.isEmpty()) {
+            baseUrl = "https://fakestoreapi.com"; // fallback if not set
+        }
+        RestAssured.baseURI = baseUrl;
+        System.out.println("Base URI set to: " + RestAssured.baseURI);
+
+        RestAssured.requestSpecification = RestAssured
+                .given()
+                .header("User-Agent", "Mozilla/5.0 (compatible; GitHub Actions)");
+    }
+}
